5a59bb508e3a2a6ceef5060ff96bf66e84203d67
Simona Sofkarovska <675351@student.inholland.nl>
2022-04-28 01:56:52 +0200

    Add project files.

---
+++ ChapeauDAL/BaseDAO.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using ChapeauModel;
+
+namespace ChapeauDAL
+{
+    public  class BaseDAO
+    {
+        private SqlDataAdapter adapter;
+        private SqlConnection connection;
+        public BaseDAO()
+        {
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["db_retake1"].ConnectionString);
+            adapter = new SqlDataAdapter();
+        }
+
+        // it opens database connection 
+        protected SqlConnection OpenConnection()
+        {
+            if (connection.State == ConnectionState.Closed || connection.State == ConnectionState.Broken)
+            {
+                connection.Open();
+            }
+
+            return connection;
+        }
+
+        private void CloseConnection()
+        {
+            connection.Close();
+        }
+
+        // executes a select queries
+        protected DataTable ExecuteSelectQuery(string query, params SqlParameter[] sqlParameters)
+        {
+            SqlCommand command = new SqlCommand();
+            DataTable dataTable;
+            DataSet dataSet = new DataSet();
+
+            try
+            {
+                command.Connection = OpenConnection();
+                command.CommandText = query;
+                command.Parameters.AddRange(sqlParameters);
+                command.ExecuteNonQuery();
+                adapter.SelectCommand = command;
+                adapter.Fill(dataSet);
+                dataTable = dataSet.Tables[0];
+            }
+            catch (SqlException exception)
+            {
+                // Print.ErrorLog(e);
+                return null;
+                throw exception;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+            return dataTable;
+        }
+
+        /* For Insert/Update/Delete Queries with transaction */
+        protected void ExecuteEditTranQuery(string query, SqlParameter[] sqlParameters, SqlTransaction sqlTransaction)
+        {
+            SqlCommand command = new SqlCommand(query, connection, sqlTransaction);
+            try
+            {
+                command.Parameters.AddRange(sqlParameters);
+                adapter.InsertCommand = command;
+                command.ExecuteNonQuery();
+            }
+            catch (Exception exception)
+            {
+                //Print.ErrorLog(e);
+                throw exception;
+            }
+        }
+
+        /* For Insert/Update/Delete Queries */
+        protected void ExecuteEditQuery(string query, SqlParameter[] sqlParameters)
+        {
+            SqlCommand command = new SqlCommand();
+
+            try
+            {
+                command.Connection = OpenConnection();
+                command.CommandText = query;
+                command.Parameters.AddRange(sqlParameters);
+                adapter.InsertCommand = command;
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException exception)
+            {
+                // Print.ErrorLog(e);
+                throw exception;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+    }
+}
---
+++ ChapeauDAL/EmployeeDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using ChapeauModel;
+using System.Collections.Generic;
+
+namespace ChapeauDAL
+{
+    public class EmployeeDAO
+    {
+        public Employee GetEmployee(string username, string password)
+        {
+            string query = $"SELECT Id, name, role, password FROM Employee WHERE Id = @username AND [password] = @password";
+
+            SqlParameter[] sqlParameters = new SqlParameter[2];
+            sqlParameters[0] = new SqlParameter("username", int.Parse(username));
+            sqlParameters[1] = new SqlParameter("password", password);
+
+            List<Employee> employees = ReadTables(ExecuteSelectQuery(query, sqlParameters));
+
+            if (employees.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return employees[0];
+            }
+        }
+
+        private List<Employee> ReadTables(DataTable dataTable)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Employee employee = new Employee();
+
+                employee.Id = (int)(dr["employeeId"]);
+                employee.Name = (string)(dr["name"]);
+                employee.Role = (Role)(dr["role"]);
+                employee.Password = (string)(dr["password"]);
+
+                employees.Add(employee);
+            }
+
+            return employees;
+        }
+    }
+}
