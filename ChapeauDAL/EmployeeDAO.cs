13cc8329d324fe7017d53db4911e7c6d800ebe2e
SimonaSofkarovska <71064367+SimonaSofkarovska@users.noreply.github.com>
2022-04-27 16:01:48 +0200

    Add files via upload

---
+++ ChapeauDAL/EmployeeDAO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ChapeauDAL
+{
+    public class EmployeeDAO
+    {
+        
+    }
+}
---
+++ ChapeauModel/Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauModel
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public TableStatus Status { get; set; }
+
+        public Table(int id, TableStatus status)
+        {
+            Id = id;
+            Status = status;
+        }
+    }
+}
